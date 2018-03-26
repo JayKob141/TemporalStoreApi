f53abe308d6c1c211f0fea76194223bb58864c8d
Jacob Meneses <jaykobyou@gmail.com>
2018-03-26 01:12:29 -0600

    Add: initial test for api

---
+++ test/api/test.js
@@ -0,0 +1,21 @@
+const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
+var axios = require('axios');
+chai.should();
+chai.use(chaiAsPromised);
+
+describe('Products', function() {
+
+  beforeEach(function() {
+
+  });
+
+  describe('Select Products', function() {
+
+    it('should do checkout correctly', function() {
+
+    });
+
+  });
+
+});
