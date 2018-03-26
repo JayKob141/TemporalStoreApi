3a8788d81a003da90040955cd872262485cac1b7
Jacob Meneses <jaykobyou@gmail.com>
2018-03-25 19:05:34 -0600

    Add: database related unit test

---
+++ test/db/test.js
@@ -0,0 +1,14 @@
+var assert = require('assert');
+
+describe('Products', function() {
+
+  describe('Select Products', function() {
+
+    it('should exists these products', function() {
+      assert.ok(false);
+    });
+
+  });
+
+});
+
