d585993f269b4e5b772cd969dcf900dd6fcec067
Jacob Meneses <jaykobyou@gmail.com>
2018-03-25 21:08:19 -0600

    Add: tests for db using chai and chai-as-promised

--- test/db/test.js
+++ test/db/test.js
@@ -1,11 +1,28 @@
-var assert = require('assert');
+const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
+var models = require('../../src/sequelize/models');
+var Product = models.Product;
+chai.should();
+chai.use(chaiAsPromised);
 
 describe('Products', function() {
 
   describe('Select Products', function() {
 
-    it('should exists these products', function() {
-      assert.ok(false);
+    it('should exists these products', function(done) {
+      var ProductCodes = [ 'TSHIRT', 'HAT', 'PANTS' ];
+
+      var promise = Product.findAndCountAll({ 
+            where:{
+                Code: ProductCodes
+            }
+      }).then( (result) => {
+            return new Promise( (resolve,reject) => {
+                resolve(result.count);
+            });
+      });
+
+      promise.should.eventually.equal(3).notify(done);
     });
 
   });
